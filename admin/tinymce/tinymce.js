b7acca65ab195a38534a127c5e92fb1edcd7947c
Niko <strijbol.niko@gmail.com>
2014-03-20 19:06:49 +0100

    Fix TinyMCE plugin for WordPress 1.9
    The current plugin broke the whole thing, so we should get this fixed for now. This is however only a temporary fix, we should eventuallty rewrite the implementation for TinyMCE 1.4.x.

--- admin/tinymce/tinymce.js
+++ admin/tinymce/tinymce.js
@@ -64,7 +64,8 @@ function insertNGGLink() {
 	}
 
 	if(window.tinyMCE) {
-        window.tinyMCE.execInstanceCommand(window.tinyMCE.activeEditor.id, 'mceInsertContent', false, tagtext);
+		tinyMCEPopup.editor.execCommand('mceInsertContent', false, tagtext);
+        //window.tinyMCE.execInstanceCommand(window.tinyMCE.activeEditor.id, 'mceInsertContent', false, tagtext);
 		//Peforms a clean up of the current editor HTML.
 		//tinyMCEPopup.editor.execCommand('mceCleanup');
 		//Repaints the editor. Sometimes the browser has graphic glitches.
